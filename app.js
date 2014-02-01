922b26fabb106309d5b0a096a438676800637205
lior <lior@lior-K55A.(none)>
2014-02-01 12:30:50 +0200

    Transformed to a nodejs project

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var http = require("http");
+var engine = require('jade');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
+app.use(require('express-blocks'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+    res.render('heb/index');
+});
+
+app.get('/what-is-it' , function(req,res) {
+	res.render('heb/what_is_it');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
