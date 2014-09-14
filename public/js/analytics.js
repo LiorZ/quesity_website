908fbd7692742e5fb21864c35633116fe65ee7bf
lior <liorz1984@gmail.com>
2014-09-14 12:30:10 +0300

    Adding analytics js code

---
+++ public/js/analytics.js
@@ -0,0 +1,14 @@
+/**
+ * Analytics event reporting code.
+ */
+
+$(document).ready(function() {
+	
+	$('.analytics-btn').click(function(ev) {
+		var btn_type = $(ev.target).attr('data-analytics')
+		if ( btn_type == undefined ) {
+			return;
+		}
+		ga('send', 'event', 'button', 'click', btn_type);
+	});
+})
